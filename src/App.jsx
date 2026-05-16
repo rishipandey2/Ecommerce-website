7afa683f3db0144c54738694ca708809e0323bf9
rishipandey2 <rishipandeyinfo@gmail.com>
2026-05-16 08:00:02 +0530

    Split checkoutHeader and add Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { HomePage } from './pages/HomePage'
-import {CheckoutPage} from './pages/CheckoutPage'
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from './pages/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
 import {Routes,Route} from 'react-router';
